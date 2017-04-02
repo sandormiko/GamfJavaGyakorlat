48b6dccfc02c7e88eb2a8b7bbbc59eae4f235349
Sándor Mikó <miko@elte.hu>
2017-04-02 16:52:18 +0200

    Initial commit

---
+++ src/hu/gamf/javagyakorlat/AfaErtek.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu.gamf.javagyakorlat;
+
+/**
+ *
+ * @author 4-11-3-Hallgato
+ */
+public enum AfaErtek {
+ 
+    _27(new Double(27)),
+    _15(new Double(15));
+    
+    private Double ertek;
+    private AfaErtek(Double ertek) {    
+        this.ertek = ertek;
+    }
+    
+    public Double getErtek(){
+        return ertek;
+    }
+    
+     public static AfaErtek getEnum(String keresendo){
+       for(AfaErtek afaErtek : values()){
+           
+           if(afaErtek.getErtek().equals(Double.valueOf(keresendo))){
+               return afaErtek;
+           }
+       }
+       throw new IllegalArgumentException("Hiba Történt, nem megfelelő enum érték");
+    }
+}
