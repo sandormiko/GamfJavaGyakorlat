ab2dea0ce16cd555f3d8fe29a3d4b21b9474d54b
sandormiko <miko@elte.hu>
2017-04-21 21:41:10 +0200

    Foreign key példa

---
+++ gyak2/02/src/hu/gamf/javagyakorlat/GamfJavaGyakorlat.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu.gamf.javagyakorlat;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author 4-11-3-Hallgato
+ */
+public class GamfJavaGyakorlat {
+
+    public String nev = "GamfJavaGyakorlat";
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws ParseException {
+        ArrayList<Szamitas> szamitasLista = new ArrayList<>();
+        AdatBazisKezelo db = new AdatBazisKezelo();
+        SzamitasService service = new SzamitasService();
+
+        Scanner scanner = new Scanner(System.in, "iso-8859-2");
+        System.out.println("Add meg az azonositojat");
+
+        String azonosito = scanner.next();
+        Szemely szemely = szemelytRegisztralHaUj(db, azonosito, scanner);
+        System.out.println("Add meg a nettó összeget");
+
+        String nettoOsszegString = scanner.next();
+
+        System.out.println("Add meg az áfa értékét");
+        String afaErtekString = scanner.next();
+        Szamitas szamitas = null;
+        try {
+            szamitas = service.szamit(nettoOsszegString, afaErtekString);
+        } catch (Exception e) {
+            System.err.println("Az áfa értékre csak 15 vagy 27 lehet");
+
+        }
+        szamitas.szemelyId = szemely.szemelyId;
+        System.out.println("Adó értéke " + szamitas.adoErtek);
+        System.out.println("Bruttó összeg " + szamitas.bruttoErtek);
+        voltEIlyenSzamitas(szamitasLista, szamitas);
+        szamitasLista.add(szamitas);
+
+        db.mentSzamitast(szamitas);
+
+        for (Szamitas sz : db.getSzamitasok()) {
+            System.out.println(sz);
+        }
+
+    }
+
+    private static Szemely szemelytRegisztralHaUj(AdatBazisKezelo db, String azonosito, Scanner scanner) throws ParseException {
+        Szemely szemely = db.getSzemelyByAzonosito(azonosito);
+        
+        if (szemely == null) {
+            szemely = new Szemely();
+            szemely.azonosito = azonosito;
+            System.out.println("Ön még nincs regisztrálva rendszerünkben");
+            System.out.println("Adja meg vezetéknevét");
+            szemely.vezeteknev = scanner.next();
+            System.out.println("Adja meg keresztnevét");
+            szemely.keresztnev = scanner.next();
+            
+            System.out.println("Adja meg születési idejét");
+            DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+            szemely.szuletesiIdo = formatter.parse(scanner.next());
+            db.mentSzemelyt(szemely);
+        }
+        
+        return szemely;
+    }
+
+    private static void voltEIlyenSzamitas(ArrayList<Szamitas> szamitasok, Szamitas ujSzamitas) {
+        for (Szamitas aktualis : szamitasok) {
+            if (aktualis.nettoOsszeg.equals(ujSzamitas.nettoOsszeg)) {
+                if (aktualis.afaErteke.equals(ujSzamitas.afaErteke)) {
+                    //System.out.println("Ilyen Számítás már volt");
+                }
+            }
+        }
+        List<Szamitas> egyezoSzamitasok = szamitasok.stream()
+                .filter(aktualis -> aktualis.nettoOsszeg.equals(ujSzamitas.nettoOsszeg) && aktualis.afaErteke.equals(ujSzamitas.afaErteke)).collect(Collectors.toList());
+        if (egyezoSzamitasok.size() > 0) {
+            System.out.println("Ilyen Számítás már volt");
+        }
+    }
+}
---
+++ gyak2/02/src/hu/gamf/javagyakorlat/Szamitas.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu.gamf.javagyakorlat;
+
+import java.util.Date;
+
+/**
+ *
+ * @author 4-11-3-Hallgato
+ */
+public class Szamitas {
+
+    public Long szamitasId;
+    public Double nettoOsszeg;
+    public Double adoErtek;
+    public AfaErtek afaErteke;
+    public Double bruttoErtek;
+    public Date letrehozasIdeje;
+    public Long szemelyId;
+
+    @Override
+    public String toString() {
+        return "Szamitas{" + "szamitasId=" + szamitasId + ", nettoOsszeg=" + nettoOsszeg + ", adoErtek=" + adoErtek + ", afaErteke=" + afaErteke + ", bruttoErtek=" + bruttoErtek + ", letrehozasIdeje=" + letrehozasIdeje + ", szemelyId=" + szemelyId + '}';
+    }
+
+   
+
+    
+
+    
+}
