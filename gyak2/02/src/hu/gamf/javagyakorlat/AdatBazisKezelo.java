807a75c681627a47402d12596d521fb4a4a5a950
sandormiko <miko@elte.hu>
2017-04-22 09:27:52 +0200

    Hibakezelés

--- gyak2/02/src/hu/gamf/javagyakorlat/AdatBazisKezelo.java
+++ gyak2/02/src/hu/gamf/javagyakorlat/AdatBazisKezelo.java
@@ -12,8 +12,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -25,7 +23,7 @@ public class AdatBazisKezelo {
     private static final String DB_USER = "sa";
     private static final String DB_USER_PASSWORD = "";
 
-    public ArrayList<Szamitas> getSzamitasok() {
+    public ArrayList<Szamitas> getSzamitasok() throws SQLException {
         ArrayList<Szamitas> lekerdezettSzamitasok = new ArrayList<>();
         Connection conn = null;
         Statement stmt = null;
@@ -35,7 +33,7 @@ public class AdatBazisKezelo {
 
             conn = DriverManager.getConnection(DB_URL, DB_USER, DB_USER_PASSWORD);
             stmt = conn.createStatement();
-            rs = stmt.executeQuery("select szamitas_id,netto_osszeg,afa,brutto_osszeg,ado_ertek, letrehozas_ideje  from szamitas");
+            rs = stmt.executeQuery("select szamitas_id,netto_osszeg,afa,brutto_osszeg,ado_ertek, letrehozas_ideje, szemely_id  from szamitas");
             while (rs.next()) {
                 Szamitas sz = new Szamitas();
                 sz.szamitasId = rs.getLong("szamitas_id");
@@ -44,21 +42,23 @@ public class AdatBazisKezelo {
                 sz.bruttoErtek = rs.getDouble("brutto_osszeg");
                 sz.adoErtek = rs.getDouble("ado_ertek");
                 sz.letrehozasIdeje = rs.getDate("letrehozas_ideje");
+                sz.szemelyId = rs.getLong("szemely_id");
                 lekerdezettSzamitasok.add(sz);
             }
             return lekerdezettSzamitasok;
-        } catch (Exception e) {
+        } catch (SQLException e) {
+            System.err.println("Hiba történt a számítás mentése során");
             e.printStackTrace();
+            throw e;
         } finally {
             close(rs);
             close(stmt);
             close(conn);
         }
-        return null;
 
     }
 
-    public void mentSzamitast(Szamitas sz) {
+    public void mentSzamitast(Szamitas sz) throws SQLException {
         Connection conn = null;
         PreparedStatement ps = null;
         try {
@@ -78,7 +78,9 @@ public class AdatBazisKezelo {
             ps.executeUpdate();
 
         } catch (SQLException e) {
+            System.err.println("Hiba történt a számítások lekérdezése során");
             e.printStackTrace();
+            throw e;
         } finally {
             close(ps);
             close(conn);
@@ -95,7 +97,7 @@ public class AdatBazisKezelo {
         }
     }
 
-    public Szemely getSzemelyByAzonosito(String azonosito) {
+    public Szemely getSzemelyByAzonosito(String azonosito) throws SQLException {
         Szemely szemely = null;
         Connection conn = null;
         PreparedStatement ps = null;
@@ -114,20 +116,22 @@ public class AdatBazisKezelo {
                 szemely.keresztnev = rs.getString("keresztnev");
                 szemely.vezeteknev = rs.getString("vezeteknev");
                 szemely.szuletesiIdo = rs.getDate("szuletesi_ido");
+
             }
             return szemely;
-        } catch (Exception e) {
+        } catch (SQLException e) {
+            System.err.println("Hiba történt a személy lekérdezése során");
             e.printStackTrace();
+            throw e;
         } finally {
             close(rs);
             close(ps);
             close(conn);
         }
-        return null;
 
     }
 
-    public void mentSzemelyt(Szemely sz) {
+    public void mentSzemelyt(Szemely sz) throws SQLException {
         Connection conn = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
@@ -137,7 +141,7 @@ public class AdatBazisKezelo {
             conn = DriverManager.getConnection(DB_URL, DB_USER, DB_USER_PASSWORD);
             ps = conn.prepareStatement("insert into szemely("
                     + "azonosito,keresztnev,vezeteknev,szuletesi_ido) "
-                    + "values(?,?,?,?)",Statement.RETURN_GENERATED_KEYS);
+                    + "values(?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
 
             ps.setString(1, sz.azonosito);
             ps.setString(2, sz.keresztnev);
@@ -152,7 +156,10 @@ public class AdatBazisKezelo {
             }
 
         } catch (SQLException e) {
+            System.err.println("Hiba történt a személy mentése során");
             e.printStackTrace();
+            throw e;
+
         } finally {
             close(rs);
             close(ps);
